3e63d98d009bd824d00ddf1672fb9dc384e5f457
dssingh02 <dssingh@princeton.edu>
2022-09-18 18:16:38 -0400

    Add plot field lines functionality

--- v2_sim_wrapper.m
+++ v2_sim_wrapper.m
@@ -215,5 +215,79 @@ end
 
 % close(vid)
 
+%% initialize solver domain - volume field lines
+
+Z_plane = 0;
+
+x_q = linspace(-2,2,100);
+y_q = linspace(-2,2,100);
+z_q = linspace(-2,2,100);
+
+[X,Y,Z] = meshgrid(x_q, y_q, z_q);
+
+domain_r = [X(:), Y(:), Z(:)];
+
+%% solve sheet - plane
+
+[B] = solve_B_iterative(r, dl, t, I, N, domain_r, mu0);
+
+%% unwrap sheet - plane
+
+B_x = B(:,1);
+B_y = B(:,2);
+B_z = B(:,3);
+
+B_x = reshape(B_x, [length(x_q), length(y_q), length(z_q)]);
+B_y = reshape(B_y, [length(x_q), length(y_q), length(z_q)]);
+B_z = reshape(B_z, [length(x_q), length(y_q), length(z_q)]);
+B_mag = sqrt(B_x.^2 + B_y.^2 + B_z.^2);
+
+B_x(B_mag > 10^-3) = NaN;
+B_y(B_mag > 10^-3) = NaN;
+B_z(B_mag > 10^-3) = NaN;
+B_mag(B_mag > 10^-3) = NaN;
+
+%% Plot results
+
+clrs = [174,118,163;25,101,176;123,175,222;144,201,135;247,240,86;241,147,45;
+    220,5,12;209,187,215;136,46,114;82,137,199;78,178,101;202,224,171;
+    246,193,65;232,96,28;119,119,119];
+clrs = clrs./255;
+
+[startX,startY,startZ] = meshgrid([-2:0.4:2],[-2:0.4:2],2);
+verts = stream3(X,Y,Z,B_x,B_y,B_z,startX,startY,startZ);
+
+[startX,startY,startZ] = meshgrid(-2,[-2:0.4:2],[-2:0.4:2]);
+verts2 = stream3(X,Y,Z,B_x,B_y,B_z,startX,startY,startZ);
+
+figure(1)
+clf;
+hold on;
+for i = 1:length(r)
+    plot3(r{i}(:,1),r{i}(:,2),r{i}(:,3));
+end
+lineobj = streamline(verts);
+lineobj2 = streamline(verts2);
+for i = 1:length(lineobj)
+    lineobj(i).LineWidth = 2;
+    lineobj(i).Color = [clrs(6,:),0.4];
+end
+for i = 1:length(lineobj2)
+    lineobj2(i).LineWidth = 2;
+    lineobj2(i).Color = [clrs(6,:),0.4];
+end
+hold off;
+xlabel('x (m)')
+ylabel('y (m)')
+zlabel('z (m)')
+view(-30,30)
+axis image;
+xlim([-2,2])
+ylim([-2,2])
+zlim([-2,2])
+
+title(sprintf('%i coils, N = %i turns, I = %.2f A, n_{shape} = %i',...
+        length(r), N(1), I(1), n))
+
 
 
